37b7115324468bfca329308e7a61b7c6a6cf76b5
Sara1502 <moreirasaragds@gmail.com>
2025-02-24 13:10:48 -0300

    add começo da função de addicionar ao carrinho

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -5,7 +5,7 @@ import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "
 import { CartContext } from "../../contexts/cart";
 
 const CartSheet = () => {
-    const {isOpen, toggleCart} = useContext(CartContext);
+    const {isOpen, toggleCart, products} = useContext(CartContext);
     return ( 
         <Sheet open={isOpen} onOpenChange={toggleCart}>
             <SheetContent>
@@ -15,9 +15,14 @@ const CartSheet = () => {
                         ngkfdjngkjd
                     </SheetDescription>
                 </SheetHeader>
+                    {products.map((product) => (
+                        <h1 key={product.id}>
+                            {product.name} - {product.quantity}
+                        </h1>
+                    ))}
             </SheetContent>
         </Sheet>
      );
-}
+};
  
 export default CartSheet;
