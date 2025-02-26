62484dacb74df72012751ef99232c13c0d26efc8
Sara1502 <moreirasaragds@gmail.com>
2025-02-26 11:47:14 -0300

    add produto no carrinho

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 
+import CartProductItem from "../../components/cart-product-item";
 import { CartContext } from "../../contexts/cart";
 
 const CartSheet = () => {
@@ -15,11 +16,9 @@ const CartSheet = () => {
                         ngkfdjngkjd
                     </SheetDescription>
                 </SheetHeader>
-                    {products.map((product) => (
-                        <h1 key={product.id}>
-                            {product.name} - {product.quantity}
-                        </h1>
-                    ))}
+                {products.map((product) => (
+                    <CartProductItem key={product.id} product={product}/>
+                ))}
             </SheetContent>
         </Sheet>
      );
